0af6d581330032e8f32c8ca96efb7ce7e856fd54
Nitin lalwani <nitindlalwani@gmail.com>
2017-11-12 00:18:07 +0530

    Add files via upload

---
+++ images/resize.py
@@ -0,0 +1,19 @@
+from PIL import Image
+import os, time
+
+size_1024 = (1024, 1024)
+size_512 = (512, 512)
+
+for f in os.listdir('.'):
+	if f.endswith('.jpg'):
+		i = Image.open(f)
+		fn, fext = os.path.splitext(f)
+		i.thumbnail(size_1024)
+		i.save('fulls/{}.jpg'.format(fn))
+		i.thumbnail(size_512)
+		i.save('thumbs/{}.jpg'.format(fn))
+		del i
+		os.remove(f)
+	
+
+
